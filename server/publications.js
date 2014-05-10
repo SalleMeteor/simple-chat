43247a38e2ccd0df3edb3a28bcb03b4e7b658fe9
gi22463 <gi22463@salle.url.edu>
2014-05-10 10:42:24 +0000

    1

---
+++ server/publications.js
@@ -0,0 +1,5 @@
+//Meteor.publish("myContacts", function(name) {
+/*Meteor.publish("allMessages", function() {
+  return Missatges.find(); // get all contacts
+  //return Contacts.find({ name: name });
+});*/
